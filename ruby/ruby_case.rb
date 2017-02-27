23025f6be900737ffd32b65c35dc134c9d46926b
Siddharth Kulkarni <sidcool1234@gmail.com>
2017-02-27 22:15:39 +0530

    Ruby challenges solved

---
+++ ruby/ruby_case.rb
@@ -0,0 +1,7 @@
+#https://www.hackerrank.com/challenges/ruby-case
+
+
+def identify_class(obj)
+  allowed_classes = ['Hacker','Submission','TestCase','Contest']	
+  puts allowed_classes.include?(obj.class.to_s) ?  "It's a #{obj.class.to_s}!" : "It's an unknown model"
+end
---
+++ src/com/sid/hr/TheLoveLetterMystery.java
@@ -0,0 +1,13 @@
+package com.sid.hr;
+
+public class TheLoveLetterMystery {
+
+	public TheLoveLetterMystery() {
+		
+	}
+	public static void main(String[] args) {
+		String str = "abc";
+		
+	}
+
+}
